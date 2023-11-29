b913ac2f51abdb6afc58ebee8b6631e4f3508220
herdonyan <37699868+herdonyan@users.noreply.github.com>
2023-11-29 12:32:41 +0800

    t

--- tabpfn/transformer.py
+++ tabpfn/transformer.py
@@ -99,8 +99,8 @@ class TransformerModel(nn.Module):
 
     def forward(self, src, src_mask=None, single_eval_pos=None):
         assert isinstance(src, tuple), 'inputs (src) have to be given as (x,y) or (style,x,y) tuple'
-        single_eval_pos = 10
-        print(src, src_mask, single_eval_pos)
+        # single_eval_pos = 10
+        print(single_eval_pos==None)
         
         if len(src) == 2: # (x,y) and no style
             src = (None,) + src
