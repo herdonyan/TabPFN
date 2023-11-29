b53328de78b6c13fca6676fe2c132762124965d9
herdonyan <37699868+herdonyan@users.noreply.github.com>
2023-11-29 23:24:53 +0800

    Update train.py

--- tabpfn/train.py
+++ tabpfn/train.py
@@ -64,8 +64,9 @@ def train(priordataloader_class, criterion, encoder_generator, emsize=200, nhid=
     if isinstance(criterion, nn.GaussianNLLLoss):
         n_out = 2
     elif isinstance(criterion, nn.CrossEntropyLoss):
-        print("Using CrossEntropyLoss", criterion, nn.CrossEntropyLoss, criterion.weight)
-        n_out = criterion.weight.shape[0]
+        # print("Using CrossEntropyLoss", criterion, nn.CrossEntropyLoss, criterion.weight)
+        # n_out = criterion.weight.shape[0]
+        n_out = 2
     else:
         n_out = 1
 
